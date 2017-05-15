a2d92370f4afcfc62787638eebbd0dbc8e0648fb
hoanki2212@gmail.com <hoannv@projectkit.org>
2017-05-16 06:14:57 +0700

    Fix bugs on order

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :ratings, dependent: :destroy
-  validates :name, presence: true
+  validates :name, length: {maximum: Settings.user.maxname}, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
